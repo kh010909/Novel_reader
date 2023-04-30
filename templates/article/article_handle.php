ad4950fa198dc41e641b154ad72edf6820ddc02a
kh010909 <95903086+kh010909@users.noreply.github.com>
2023-04-30 17:49:43 +0800

    Update article_handle.php

--- templates/article/article_handle.php
+++ templates/article/article_handle.php
@@ -2,13 +2,26 @@
 include('../core/config.php');
 session_start();
 if (isset($_GET['nId']) & isset($_GET['chapter'])) {
+    $id = $_GET['nId'];
+    $chapter = $_GET['chapter'];
     if (isset($_SESSION["user"])) {
         $user_id = $_SESSION["user"]["uId"];
+        //if there is no record
+        $sql = "SELECT * FROM `bookrecord` WHERE `nId` = $id AND `uId` = $user_id";
+        $temp = get_row($sql, $count, $sql_link);
+        //there's no record
+        if ($count == 0) {
+            $sql = "INSERT INTO `bookrecord` VALUES (NULL,$id,$user_id,0,$chapter,'watch')";
+            $temp = $sql_link->query($sql);
+        }
+        //else: update record
+        else {
+            $sql = "UPDATE `bookrecord` SET `currCh` = $chapter WHERE `nId` = $id AND `uId` = $user_id";
+            $temp = $sql_link->query($sql);
+        }
     } else {
         $user_id = 0;
     }
-    $id = $_GET['nId'];
-    $chapter = $_GET['chapter'];
     $sql = "SELECT * FROM `article` WHERE `nId` = $id AND `aChapter` = $chapter";
     $article_row = get_row($sql, $articlecount, $sql_link);
     $article_row[0]['aContent'] = strip_tags($article_row[0]['aContent'], '<br>');
@@ -18,19 +31,7 @@ if (isset($_GET['nId']) & isset($_GET['chapter'])) {
     $_SESSION['totalarticlecount'] = $totalarticlecount;
     $_SESSION['chapter'] = $chapter;
     $_SESSION['id'] = $id;
-    //if there is no record
-    $sql = "SELECT * FROM `bookrecord` WHERE `nId` = $id AND `uId` = $user_id";
-    $temp = get_row($sql, $count, $sql_link);
-    //there's no record
-    if ($count == 0) {
-        $sql = "INSERT INTO `bookrecord` VALUES (NULL,$id,$user_id,0,$chapter,'watch')";
-        $temp = $sql_link->query($sql);
-    }
-    //else: update record
-    else {
-        $sql = "UPDATE `bookrecord` SET `currCh` = $chapter WHERE `nId` = $id AND `uId` = $user_id";
-        $temp = $sql_link->query($sql);
-    }
+
 ?>
     <script>
         window.location.href = '../article.php';
