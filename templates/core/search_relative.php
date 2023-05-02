60a3a40e631a475c96b9fd21f7b06e974c3fd011
5241513 <96127309+5241513@users.noreply.github.com>
2023-05-03 00:27:27 +0800

    Update search_relative
    add completed state search and
    collection part to search bar function
    -> but there might be error when using collection function(login state) (my sql hadn't update, so can't test)

--- templates/core/search_relative.php
+++ templates/core/search_relative.php
@@ -9,7 +9,13 @@ function search_list($sql_link, $type = NULL, &$length = 0, $target = null, $lim
     $sql = null;
     if (isset($target)) { //get data from novel table
         if ($type == "SEARCH") {
-            $sql = "SELECT DISTINCT novel.* FROM novel NATURAL JOIN tag WHERE tag LIKE '%$target%' OR nName LIKE '%$target%' ";
+            $sql = "SELECT DISTINCT novel.* FROM novel NATURAL JOIN tag WHERE tag LIKE '%$target%' OR nName LIKE '%$target%' OR completed = '$target'";
+            if (isset($cur_user)) {
+                $sql .= " OR novel.nId IN (
+                    SELECT b.nId FROM bookrecord b, keep k WHERE b.bId = k.bId AND collectId IN (
+                        SELECT collectId FROM user u, collection c WHERE u.uId = c.uId AND u.uId = '$cur_user' AND c.collectName LIKE '%$target%')
+                )";
+            }
         } else if ($type == "TAG") {
             $sql = "SELECT * FROM novel n NATURAL JOIN tag t WHERE tag LIKE '%$target%'";
         } else if ($type == "COLLECTION" && isset($object_user)) {
