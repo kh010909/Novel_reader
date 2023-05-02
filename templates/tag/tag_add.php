c01614c9ec447a363185454f21c521e7679bbf56
5241513 <96127309+5241513@users.noreply.github.com>
2023-05-02 23:08:33 +0800

    Fix member_handle + tag add

--- templates/tag/tag_add.php
+++ templates/tag/tag_add.php
@@ -2,14 +2,15 @@
 include('../core/config.php');
 session_start();
 if (isset($_POST['nId'])) {
+    $id = $_POST['nId'];
     if (isset($_SESSION["user"])) {
-        $id = $_POST['nId'];
+
         $tag = $_POST['tag'];
         $tag = $sql_link->quote($tag);
         $sql = "INSERT INTO `tag` SELECT $id,$tag FROM DUAL WHERE NOT EXISTS(SELECT nId,tag FROM `tag` WHERE nId = $id AND tag = $tag)";
         $temp = $sql_link->query($sql);
     }
-?>
+    ?>
     <script>
         window.location.href = '../novel/novel_handle.php?nId=<?= $id ?>';
     </script>
